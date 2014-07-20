af951769579e79a9e08de6f6eb88cdf7454e90d5
Yufeng Wang <yufeng05w@gmail.com>
2014-07-20 15:05:49 -0400

    migration: add columns to events table

---
+++ app/migrations/002_add_event_columns.php
@@ -0,0 +1,24 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Migration_Add_event_columns extends CI_Migration {
+
+  public function up() {
+    $fields = array(
+      'meeting' => array(
+        'type' => 'BOOL',
+        'null' => 'FALSE'
+      ),
+      'community_service' => array(
+        'type' => 'BOOL',
+        'null' => 'FALSE'
+      )
+    );
+
+    $this->dbforge->add_column('events', $fields);
+  }
+
+  public function down() {
+    $this->dbforge->drop_column('events', 'meeting');
+    $this->dbforge->drop_column('events', 'community_service');
+  }
+}
