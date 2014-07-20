dd3fbed15b5f9036a4f288f8a19744cad88d67f6
Yufeng Wang <yufeng05w@gmail.com>
2014-07-20 17:38:50 -0400

    add pillars to homepage

--- app/views/home_view.php
+++ app/views/home_view.php
@@ -12,8 +12,28 @@
 </div>
 </div>
 
+<div id="pillar-container">
 <div class="row">
-  <div class="small-10 columns">
-    <p>Page rendered in <strong>{elapsed_time}</strong> seconds</p>
+  <div class="small-12 medium-4 columns">
+    <div class="pillar">
+      <img src="<?=base_url()?>public/img/pillar-professionalism.png" width="200px" />
+      <h3>Professionalism</h3>
+      <p>Take advantage of workshops, panels, meetings, and conferences to connect and network with companies, fellow interns, and SASE members.</p>
+    </div>
   </div>
+  <div class="small-12 medium-4 columns">
+    <div class="pillar">
+      <img src="<?=base_url()?>public/img/pillar-diversity.png" width="200px" />
+      <h3>Diversity</h3>
+      <p>Explore cultural diversity in the workplace and campus. Get involved with the Asian American community and engineering/science activities.</p>
+    </div>
+  </div>
+  <div class="small-12 medium-4 columns">
+    <div class="pillar">
+      <img src="<?=base_url()?>public/img/pillar-service.png" width="200px" />
+      <h3>Service</h3>
+      <p>Give back to the community, while having lots of fun as a group. Participate in volunteer work around the Northeastern campus and Boston.</p>
+    </div>
+  </div>
+</div>
 </div>
