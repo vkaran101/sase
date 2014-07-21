7dc6ce39ec60424b951667680233a3f4368f5685
Yufeng Wang <yufeng05w@gmail.com>
2014-07-20 21:17:46 -0400

    start events page layout

--- app/controllers/events.php
+++ app/controllers/events.php
@@ -2,21 +2,27 @@
 
 class Events extends CI_Controller {
 
-  /**
-   * Maps to the following URL
-   *    http://example.com/index.php/events
-   *    http://example.com/index.php/events/index
-   *
-   * @see http://codeigniter.com/user_guide/general/urls.html
-   */
+  public function __construct()
+  {
+    parent::__construct();
+    $this->load->helper('url');
+  }
+
   public function index()
   {
-    $this->load->helper('url');
-
     $data['title'] = 'Events | Northeastern SASE';
 
-    $this->load->view('templates/header',$data);
+    $this->load->view('templates/header', $data);
     $this->load->view('events_view');
+    $this->load->view('templates/footer');
+  }
+
+  public function past()
+  {
+    $data['title'] = 'Past Events | Northeastern SASE';
+
+    $this->load->view('templates/header', $data);
+    $this->load->view('past_events_view');
     $this->load->view('templates/footer');
   }
 }
--- app/views/events_view.php
+++ app/views/events_view.php
@@ -1,9 +1,56 @@
-<div id="container">
-  <h1>Epic Events</h1>
-
-  <div id="body">
-    Events Page
+<div class="banner">
+<div class="row">
+  <div class="small-12 medium-10 medium-centered columns">
+    <h2>Events</h2>
+    <h4 class="subheader">Find upcoming events, meetings, and community service.</h4>
   </div>
+</div>
+</div>
 
-  <p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds</p>
+<div class="row">
+  <div class="small-12 medium-6 columns">
+    <ul class="events-table">
+      <li class="title">Upcoming</li>
+      <li class="entry">
+        <h4>event title</h4>
+        <div class="event-info">
+          <i class="fa fa-fw fa-calendar"></i><span>9/25/14</span>
+          <i class="fa fa-fw fa-clock-o"></i><span>18:35</span>
+          <i class="fa fa-fw fa-map-marker"></i><span>348 Curry Student Center</span>
+        </div>
+        <p>event description</p>
+      </li>
+    </ul>
+  </div>
+  <div class="small-12 medium-6 columns">
+    <div class="row">
+      <div class="small-12 columns">
+        <ul class="events-table">
+          <li class="title">General Meetings</li>
+          <li class="entry"></li>
+        </ul>
+      </div>
+    </div>
+    <div class="row">
+      <div class="small-12 columns">
+        <ul class="events-table">
+          <li class="title">Community Service</li>
+          <li class="entry"></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="bottom-banner">
+<div class="row">
+  <div class="small-12 medium-10 medium-centered columns">
+    <div class="small-12 medium-7 columns text-center">
+      <p>Check out our past events to see what we have done!</p>
+    </div>
+    <div class="small-12 medium-5 columns text-center">
+      <a href="<?=base_url()?>events/past" class="button radius">Past Events</a>
+    </div>
+  </div>
+</div>
 </div>
