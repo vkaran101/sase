25a0fe5c11bd1cf85cd6a04c5a067c751d78de13
Yufeng Wang <yufeng05w@gmail.com>
2014-09-01 13:50:18 -0400

    initial changes to sponsors page

--- app/controllers/sponsors.php
+++ app/controllers/sponsors.php
@@ -2,24 +2,16 @@
 
 class Sponsors extends CI_Controller {
 
-  /**
-   * Maps to the following URL
-   *    http://example.com/index.php/sponsors
-   *    http://example.com/index.php/sponsors/index
-   *
-   * @see http://codeigniter.com/user_guide/general/urls.html
-   */
   public function index()
   {
     $this->load->helper('url');
 
-    $data['title'] = 'Sponsors | Northeastern SASE';
+    $this->data['title'] = 'Sponsors | Northeastern SASE';
 
-    $this->load->view('templates/header',$data);
-    $this->load->view('sponsors_view');
+    $this->load->view('templates/header',$this->data);
+    $this->load->view('sponsors');
     $this->load->view('templates/footer');
   }
 }
 
-/* End of file sponsors.php */
-/* Location: ./app/controllers/sponsors.php */
+/* End of file */
---
+++ app/views/sponsors.php
@@ -0,0 +1,61 @@
+<div class="banner clear-gap">
+<div class="row">
+  <div class="small-12 medium-10 medium-centered columns">
+    <h2>National Sponsors</h2>
+    <h4 class="subheader">Organizations that support SASE at the national level.</h4>
+  </div>
+</div>
+</div>
+
+<div class="thumbnail-block" id="gold">
+<div class="row">
+  <div class="small-12 medium-8 medium-centered columns text-center">
+    <h3>Gold Sponsors</h3>
+    <ul class="small-block-grid-2 medium-block-grid-3">
+      <li><a href="http://www.boozallen.com/careers" target="_blank">
+        <img src="<?=base_url()?>public/img/gold/bah.png"></a>
+      </li>
+      <li><a href="http://www.ge.com/careers" target="_blank">
+        <img src="<?=base_url()?>public/img/gold/ge.png"></a>
+      </li>
+      <li><a href="http://www.itw.com/careers/" target="_blank">
+        <img src="<?=base_url()?>public/img/gold/itw.png"></a>
+      </li>
+      <li><a href="http://www.pg.com/en_US/careers/index.shtml" target="_blank">
+        <img src="<?=base_url()?>public/img/gold/png.png"></a>
+      </li>
+      <li><a href="http://www.shell.com/global/aboutshell/careers.html" target="_blank">
+        <img src="<?=base_url()?>public/img/gold/shell.png"></a>
+      </li>
+      <li><a href="http://www.toyota.com/usa/careers/" target="_blank">
+        <img src="<?=base_url()?>public/img/gold/toyota.png"></a>
+      </li>
+    </ul>
+  </div>
+</div>
+</div>
+
+<div class="thumbnail-block" id="silver">
+<div class="row">
+  <div class="small-12 medium-8 medium-centered columns text-center">
+    <h3>Silver Sponsors</h3>
+    <ul class="small-block-grid-2 medium-block-grid-3">
+      <li><a href="https://www.cra.com/careers/jobs-internships.asp" target="_blank">
+        <img src="<?=base_url()?>public/img/silver/charles.png"></a>
+      </li>
+      <li><a href="http://www.lockheedmartinjobs.com/index.aspx" target="_blank">
+        <img src="<?=base_url()?>public/img/silver/lockheed.png"></a>
+      </li>
+      <li><a href="http://www.michelincareers.com/us/eng" target="_blank">
+        <img src="<?=base_url()?>public/img/silver/michelin.png"></a>
+      </li>
+      <li><a href="http://jobs.owenscorningcareers.com/" target="_blank">
+        <img src="<?=base_url()?>public/img/silver/owens.png"></a>
+      </li>
+      <li><a href="http://www.southerncompany.com/about-us/careers/home.cshtml" target="_blank">
+        <img src="<?=base_url()?>public/img/silver/southern.png"></a>
+      </li>
+    </ul>
+  </div>
+</div>
+</div>
