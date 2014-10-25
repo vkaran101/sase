32e77a10d045e09eda0760638345ee8cbad941cb
Yufeng Wang <yufeng05w@gmail.com>
2014-10-25 19:47:28 -0400

    upgrade site footer

--- app/views/templates/footer.php
+++ app/views/templates/footer.php
@@ -1,58 +1,17 @@
 <footer>
 <div class="row">
-  <div class="small-12 medium-3 columns hide-for-small">
-    <a href="<?=base_url()?>"><img src="<?=base_url()?>public/img/sase_stamp_light.png"></a>
+  <div class="small-12 medium-3 medium-push-3 columns small-only-text-center">
+    <p>Subscribe to our mailing list and get awesome updates right in your mailbox!</p>
+    <a href="<?=base_url()?>newsletter" class="button small radius">Sign Up</a>
   </div>
-  <div class="small-12 medium-3 columns">
-    <div class="row">
-      <div class="small-12 columns text-center">
-        <p>Subscribe to our mailing list and get awesome updates!</p>
-        <a href="<?=base_url()?>newsletter" class="button small">Sign Up</a>
-      </div>
-      <hr class="divider show-for-small" />
-      <div class="small-12 columns text-center">
-        <p>Get involved. Join our SASE chapter or become a national member for free!</p>
-        <a href="<?=base_url()?>join" class="button small">Join SASE</a>
-      </div>
-      <hr class="divider show-for-small" />
-    </div>
+  <div class="small-12 medium-3 medium-push-3 columns small-only-text-center">
+    <p>Get involved. Join our SASE chapter or become a national member for free!</p>
+    <a href="<?=base_url()?>join" class="button small radius">Join SASE</a>
+    <hr class="divider" />
   </div>
-  <div class="small-12 medium-3 columns">
-    <h3>Connect</h3>
-    <ul class="footer-side-nav">
-      <li>
-        <a href="mailto:northeastern.sase@gmail.com">
-          <span class="fa-stack fa-lg">
-            <i class="fa fa-square-o fa-stack-2x"></i>
-            <i class="fa fa-envelope fa-stack-1x"></i>
-          </span>
-          Email Us
-        </a>
-      </li>
-      <li>
-        <a href="https://www.facebook.com/NortheasternSASE" target="_blank">
-          <span class="fa-stack fa-lg">
-            <i class="fa fa-square-o fa-stack-2x"></i>
-            <i class="fa fa-facebook fa-stack-1x"></i>
-          </span>
-          Follow on Facebook
-        </a>
-      </li>
-      <li>
-        <a href="https://orgsync.com/67847/chapter" target="_blank">
-          <span class="fa-stack fa-lg">
-            <i class="fa fa-square-o fa-stack-2x"></i>
-            <i class="fa fa-repeat fa-stack-1x"></i>
-          </span>
-          Find on OrgSync
-        </a>
-      </li>
-    </ul>
-  </div>
-  <hr class="divider show-for-small" />
-  <div class="small-12 medium-3 columns">
+  <div class="small-6 medium-3 medium-pull-6 columns small-only-text-center">
     <h3>Affiliations</h3>
-    <ul class="footer-side-nav">
+    <ul>
       <li><a href="http://saseconnect.org">SASE National</a></li>
       <li><a href="http://saseneregion.weebly.com">SASE Northeast</a></li>
       <li><a href="http://www.northeastern.edu">Northeastern</a></li>
@@ -61,8 +20,37 @@
       <li><a href="http://www.northeastern.edu/aac/paac/index.html">PAAC</a></li>
     </ul>
   </div>
-  <hr class="divider show-for-small" />
-  <div class="small-6 small-centered columns show-for-small">
+  <div class="small-6 medium-3 columns small-only-text-center">
+    <h3>Connect</h3>
+    <ul>
+      <li><a href="mailto:northeastern.sase@gmail.com">
+        <span class="fa-stack">
+          <i class="fa fa-square-o fa-stack-2x"></i>
+          <i class="fa fa-envelope fa-stack-1x"></i>
+        </span>
+        Email Us</a>
+      </li>
+      <li><a href="https://www.facebook.com/NortheasternSASE" target="_blank">
+        <span class="fa-stack">
+          <i class="fa fa-square-o fa-stack-2x"></i>
+          <i class="fa fa-facebook fa-stack-1x"></i>
+        </span>
+        Follow Us</a>
+      </li>
+      <li><a href="https://orgsync.com/67847/chapter" target="_blank">
+        <span class="fa-stack">
+          <i class="fa fa-square-o fa-stack-2x"></i>
+          <i class="fa fa-repeat fa-stack-1x"></i>
+        </span>
+        Join Us</a>
+      </li>
+    </ul>
+    <hr class="divider" />
+  </div>
+</div>
+
+<div class="row">
+  <div class="small-5 small-centered columns show-for-small-only">
     <a href="<?=base_url()?>"><img src="<?=base_url()?>public/img/sase_stamp_light.png"></a>
   </div>
 </div>
