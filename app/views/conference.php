b12f6ffcfd0cf7f4ccc9ca0cacdf4b471acbb5b3
Michael <michaelanhu@gmail.com>
2016-02-08 23:55:26 -0500

    workshop update

--- app/views/conference.php
+++ app/views/conference.php
@@ -1,34 +1,29 @@
 <section class="main">
-<div class="container grey">
+<div class="container white">
 <center>
-<h2 class="blue-text">Registration is now open!</h2>
+<h2 class="blue-text">Registration is open!</h2>
 <div class="row body-row">
-<p><b>Registration closes 02/12!</b></p>
+<p><b>But it closes 02/12, so be quick!</b></p>
 <a href="https://commerce.cashnet.com/SFNESASE2016" target="_blank" class="button btn-outline large radius">Register</a>
 </div>
 </center>
-</div>
-
-  <div class="container">
-    <div class="row body-row">
-    <center>
-      <!-- <h2 class="blue-text">Get Excited!</h2> -->
-
-    <h3><b class="blue-text">2016 SASE Northeast Regional Conference</b>
-    <br>“Collaborate, Create, Inspire: We Are The Solution”</h3>
-    </center>
-    <p><b>Hosted by: Northeastern University SASE with Co-Host Northeastern University College of Engineering
-    <br>Assisted by: Sponsoring Chapters Boston University, Binghamton University, Drexel University, New York 
-    University and Stevens Institute of Technology</b></p>
-    <p>The 6th Annual SASE Northeast Regional Conference is being held at Northeastern University this year on February 13th, 2016.</p>
-    <p>The theme for this conference centers around the idea that in this day and age working with a team of individuals becomes increasingly important as you look to progress. No matter what career path that we choose, we will always be working in multidisciplinary teams. The conference will feature breakout sessions, each uniquely centered around the key ideas of “Collaborate,” “Create,” and “Inspire.”</p>
-    <p>The day will be filled with workshops, panels, keynote speakers and limitless opportunities to network with other young professionals.</p>
-    <center>
-      <p>Save the date for <b>February 13th, 2016</b>!</p>
-      <p><b>#SASENERC2016 #WeAreTheSolution</b></p>
-    </center>
+  </div>
+    <div class="container grey">
+      <div class="row body-row">
+      <center>
+        <h3><b class="blue-text">2016 SASE Northeast Regional Conference</b>
+        <br>“Collaborate, Create, Inspire: We Are The Solution”</h3>
+      </center>
+      <p><b>Hosted by: Northeastern University SASE with Co-Host Northeastern University College of Engineering
+      <br>Assisted by: Sponsoring Chapters Boston University, Binghamton University, Drexel University, New York 
+      University and Stevens Institute of Technology</b></p>
+      <p>The 6th Annual SASE Northeast Regional Conference is being held at Northeastern University this year on February 13th, 2016.</p>
+      <p>The theme for this conference centers around the idea that in this day and age working with a team of individuals becomes increasingly important as you look to progress. No matter what career path that we choose, we will always be working in multidisciplinary teams. The conference will feature breakout sessions, each uniquely centered around the key ideas of “Collaborate,” “Create,” and “Inspire.”</p>
+      <p>The day will be filled with workshops, panels, keynote speakers and limitless opportunities to network with other young professionals.</p>
+      <center>
+        <p>Save the date for <b>February 13th, 2016</b>!</p>
+        <p><b>#SASENERC2016 #WeAreTheSolution</b></p>
+      </center>
     </div>
   </div>
 </center>
-
-
--- app/views/conference/programming.php
+++ app/views/conference/programming.php
@@ -42,7 +42,7 @@
       <tr>
         <td>8:50AM - 9:40AM</td>
         <td>
-          <span data-tooltip aria-haspopup="true" class="has-tip" title="8:55-9:10 Opening Remarks<br />9:10-9:40 Opening Keynote">
+          <span data-tooltip aria-haspopup="true" class="has-tip" title="8:50-9:10 Opening Remarks<br />9:10-9:40 Opening Keynote">
             Morning Speaker & Opening
           </span>
         </td>
@@ -59,7 +59,7 @@
         <td>Classrooms</td>
       </tr>
       <tr>
-        <td>12:10PM - 12:55PM</td>
+        <td>12:10PM - 1:00PM</td>
         <td>Lunch</td>
         <td>Curry Indoor Quad + Mezzanine</td>
       </tr>
@@ -71,11 +71,16 @@
       <tr>
         <td>2:15PM-4:00PM</td>
         <td>
-          <span data-tooltip aria-haspopup="true" class="has-tip" title="2:15-2:35 Performances<br />2:35-2:50 Introductions<br />2:50-3:35 Keynote<br />3:35-4:00 Closing Remarks">
+          <span data-tooltip aria-haspopup="true" class="has-tip" title="2:15-2:35 Performances<br />2:35-2:45 Introductions<br />2:45-3:30 Keynote<br />3:30-4:00 Closing Remarks">
             Keynote Speaker & Closing
           </span>
         </td>
         <td>Blackman Auditorium</td>
+      </tr>
+      <tr>
+        <td>4:00PM - 5:00PM </td>
+        <td>Marketing Workshop</td>
+        <td>Curry 333</td>
       </tr>
       <tr>
         <td>4:00PM - 6:00PM </td>
@@ -84,8 +89,12 @@
       </tr>
       <tr>
         <td>4:00PM - 6:00PM </td>
-        <td>Golden Sponsor Suites</td>
-        <td>Curry Student Center Rooms</td>
+        <td>
+          <span data-tooltip aria-haspopup="true" class="has-tip" title="Jacobs: Curry 433<br />Boston Scientific: Curry 435">
+            Gold Sponsor Suites
+          </span>
+        </td>
+        <td>Curry 433 and 435</td>
       </tr>
       <tr>
         <td>4:00PM - 6:00PM </td>
@@ -107,17 +116,12 @@
         <td>Curry Indoor Quad</td>
       </tr>
       <tr>
-        <td>1:00PM - 6:00PM</td>
+        <td>10:00AM - 2:00PM</td>
         <td>
           <span data-tooltip aria-haspopup="true" class="has-tip" title="Come in and receive constructive feedback on your resume from professionals.">
             Resume Reviews
           </span>
         </td>
-        <td>Curry Rooms</td>
-      </tr>
-      <tr>
-        <td>5:00PM - 6:00PM</td>
-        <td>Company Suites</td>
         <td>Curry Rooms</td>
       </tr>
     </table>
@@ -127,7 +131,7 @@
 
 <div class="container light-grey" id="workshops">
   <div class="row body-row">
-    <center><h3>Workshops*</h3></center>
+    <center><h3>Workshops</h3></center>
 
     <ol class="accordion" data-accordion>
       <li class="accordion-navigation">
@@ -136,20 +140,23 @@
           <br><i>Learn the importance of working in a team of people with diverse skills and experiences to solve multifaceted problems.</i>
         </a>
         <div id="breakout1" class="content">
-          <b>Optimizing Your Team</b>
-          <br>How do you go about starting a team for what you want to do? What can you do to create a successful team? The most important element in starting a team is undertanding your "niche", or what your best quality is for your group. Come to this workshop to learn about group mentality, and how your team can succeed.
+          <b>The Road Not Taken: The Many Destinations of the STEM Degree</b><br>
+          <i>NAAAP and SASE Pro, moderated by George Luo; SN 108</i>
+          <br>75% of US News' 100 Best Jobs of 2016 are located in STEM-related fields. With such an overwhelming array of industries, professions, and careers to choose from, how can one possibly choose?
+          <br>Sit down with our selection of early-to-mid career professionals from a variety of different STEM fields and have a candid conversation about what their job is like. How did they choose their job? What do they enjoy the most about where they work? And what makes them itch to leave?
+          <br>We hope that not only will you carry away the right answers to your career questions, but that you will carry away the right questions to ask as you find your own path "less traveled by."
           <hr>
-          <b>Engineering Design Process - Brainstorming</b>
-          <br>We all suffer from a little design block from time to time. Learn how to get the creative juices flowing, and come together with others to come up with unique and intuitive designs.
+          <b>Success in Corporate R&D</b><br>
+          <i>Boston Scientific; RB 107</i>
+          <br>This workshop will focus on finding success in corporate R&D, through focusing efforts where they will deliver the most impactful results. Learn more about developing your expertise, finding your niche, and adding value. Also learn more about the process of developing new product, working with the realities of cost, performance, and schedule limitations. We will also touch upon the product development pipeline, starting with broad concepts, and progressing through downselection procedures. 
           <hr>
-          <b>Product Development</b>
-          <br>Take a detailed look into the product development process, and what each stage of the project requires from its team.
+          <b>6E’s: Redefine your leadership</b><br>
+          <i>Irene Ng, Virak Ngauv; McLeod Suites (CSC 318,320,322)</i>
+          <br>You probably have heard of or taken other leadership assessments, or models out there, such as the Myers-Briggs Type Indicator or Strengthsfinders. However, what makes our LSA (Leadership Self Assessment) different, is that we focus on continuous reflection and direct feedback from others to help track the growth of your leadership development. With the SASE 6E leadership model (Empower, Energize, Envision, Engage, Execute, Elastic), you can redefine and recognize the way you see leadership, as well as identify which of your leadership styles are stronger than others. Because when these 6 powers combine, they create senSASEtional leaders! 
           <hr>
-          <b>A Different Approach to Your Design</b>
-          <br>When is it time for us to pivot and choose a new design? While many of us have attachment issues with our design, part of being a designer is adding onto our structure, and using different perspectives to assess our pitfalls and strengths. 
-          <hr>
-          <b>6E’s: Redefine your leadership</b>
-          <br>You probably have heard of or taken other leadership assessments, or models out there, such as the Myers-Briggs Type Indicator or Strengthsfinders. However, what makes our LSA (Leadership Self Assessment) different, is that we focus on continuous reflection and direct feedback from others to help track the growth of your leadership development. With the SASE 6E leadership model (Empower, Energize, Envision, Engage, Execute, Elastic), you can redefine and recognize the way you see leadership, as well as identify which of your leadership styles are stronger than others. Because when these 6 powers combine, they create senSASEtional leaders!
+          <b>The "Everything Else": At the Intersection of Social Justice and Technology</b><br>
+          <i>ECAASU; EL 312</i>
+          <br>Innovations in STEM have introduced unprecedented changes in our world and our communities. While we celebrate such breakthroughs, we as people invested in STEM also need to understand the potential repercussions that our work can cumulatively have on our world and our communities. In this session, we will discuss the "everything else": instances where politics, economics, finance, ethics, disparities, equity, and public policy intersect with STEM. By examining case studies and the themes they exemplify, this session will bring a stronger awareness of our impacts, good and bad, as we continue to make progress in STEM.
         </div>
       </li>
       <li class="accordion-navigation">
@@ -158,21 +165,25 @@
           <br><i>Participate in interactive workshops that encourage creativity, independent thinking and coming up with original ideas.</i>
         </a>
         <div id="breakout2" class="content">
-          <b>Developing Your Online Profile</b>
-          <br>Professionals are more interconnected than ever through not only LinkedIn, but through various other online profiles such as Twitter, AngelList, and personal websites. Learn how to develop your online profile and improve your online footprint.
-          <hr>
-          <b>Why choose R&D?</b>
-          <br>Learn about the differences between researching a university, vs. researching at a firm. R&D is a great route for scientists who may not want to go into academia; come to this workshop to learn all about it!
-          <hr>
-          <b>Panel: Start Up Panel</b>
-          <br>Everything you have ever wondered about a start-up, debunked. This panel of entrepreneurs will detail their own involvement in startups, as well as tips and tricks to those new to the game.
-          <hr>
-          <b>3D Printing</b>
-          <br>Learn the basics of 3D printing! Attendees will learn how to use SolidWorks and create their own design. This workshop has limited space, up to 30 people only.
-          <hr>
-          <b>Auditing Algorithms: Towards Transparency in the Age of Big Data</b>
+          <b>Auditing Algorithms: Towards Transparency in the Age of Big Data</b><br>
+          <i>Prof. Christo Wilson; DG 050</i>
           <br>In recent years, there has been growing awareness of powerful algorithms that mediate our daily lives. For example,  Facebook personalizes the information shown to each user. While making our lives much simpler, opaque algorithms can lead to harmful outcomes. In this workshop, we will discuss our work towards "auditing algorithms", where we use carefully controlled experiments to understand the data and algorithms used by companies such as Google and Uber, increase transparency, and inform societal discussions about how our personal data is being collected and used.
+          <b>3D Printing Workshop</b><br>
+          <i>Jiaqing Qiu; SN 368</i>
+          <br>The popularity and awareness of 3D Printing is exploding. It is breaking down barriers in design and manufacturing, and unlock the potential in every person to create, innovate and fabricate 
+          <br>In this workshop we'll give basic understanding: How to create 3D geometry; how does 3D-printer work; what are the different types of printers and materials to use; what can I print with a 3D-printer; what is 3D printing limitations;
+          <br>Participants will gain exposure to the main facts of initializing and set up the 3D Printer with basic settings during the workshop, 3D modeling, and 3D printing during the workshop.
           <hr>
+          <b>Early-Stage Advice for New Startup Founders</b><br>
+          <i>MassChallenge; McLeod Suites (CSC 318,320,322)</i>
+          <br>Join MassChallenge and several MassChallenge alumni founders to discuss some common challenges to new founders. This is a great opportunity to ask questions of entrepreneurs who have been through (and are still going through!) ups and downs of starting a startup. How do you build a team? Should you apply to an accelerator? How do you gain momentum before launching a product? Find out more in this interactive panel.
+          <hr>
+          <b>Product Development</b><br>
+          <i>Fikst Product Development; SN 108</i>
+          <br>Take a detailed look into the product development process, and what each stage of the project requires from its team. The workshop will walk through Fikst's product design process, with specific examples of work done by Fikst Product Development. Emphasis will be placed on creating prototypes early and often to learn quickly and iterate on design concepts.
+          <hr>
+          <b>High School Workshop</b><br>
+          <i>Boston Unversity SASE; CSC 333</i>
         </div>
       </li>
       <li class="accordion-navigation">
@@ -181,59 +192,37 @@
           <br><i>How can one create a lasting impact? Panelists share their stories and inspire attendees to pursue their goals.</i>
         </a>
         <div id="breakout3" class="content">
-          <b>Panel: Finding Your Asian American Identity as a Professional</b>
-          <br>Having lived under the mask of "Model Minority" in school, what is it like to finally end up as a professional after years of Model Minority stereotypes? What challenges do we face as Asian-Americans in the workplace? Come to this workshop to learn more about how your Asian American identity affects you as a professional
+          <b>The Greatest Gift in Life</b><br>
+          <i>Matt Diaz; SN 108</i>
+          <br>Do you feel that there are things holding you back from what you want to achieve? Do you believe that you are capable and ready to move on to the next level in life? Do you have big hairy audacious goals? No matter where you are at, this workshop is designed to help you realize that everything you need is already inside of you. Learn how to gain clarity for what you want, and action steps to go after it.
           <hr>
-          <b>Assertive Communications</b>
-          <br>The workshop will focus on four types of communication: Aggressive, passive-aggressive, submissive, assertive. Pros and cons will be explored for each of these types of communication. The workshop will focus on the importance of assertive communications and how it helps build personal and professional relationships, while allowing tasks to be completed with a "win-win" in mind. The workshop ends with an interactive activity which allows the audience to put into action the skills they've learned in the workshop.
-          <hr>
-          <b>Where Do My Passions Intersect?</b>
+          <b>Where Do My Passions Intersect?</b><br>
+          <i>Prof. Kate Ziemer; RB 109</i>
           <br>As a professional, your career should not seem like a "chore", but should be something fulfilling and important to you. Come to this workshop to learn how you can take joy in your work through interweaving your passions in your day-to-day work life.
           <hr>
-          <b>Problems Our Generation Must Face</b>
-          <br>Learn about what problems our generation is facing, and what you can do to help through the vehicles of science and engineering!
+          <b>Assertive Communications</b><br>
+          <i>GE Aviation; EL 312</i>
+          <br>The workshop will focus on four types of communication: Aggressive, passive-aggressive, submissive, assertive. Pros and cons will be explored for each of these types of communication. The workshop will focus on the importance of assertive communications and how it helps build personal and professional relationships, while allowing tasks to be completed with a "win-win" in mind. The workshop ends with an interactive activity which allows the audience to put into action the skills they've learned in the workshop.
           <hr>
-          <b>Panel: Academia vs. Industry</b>
-          <br>It is often said that your career diverges into two paths; academia and industry. Come to this panel to learn more about each path, and which one may be a better career option for you!
+          <b>Finding Your Identity as an Asian-American Professional</b><br>
+          <i>Various; DG 050</i>
+          <br>Having lived under the mask of "Model Minority" in school, what is it like to finally end up as a professional after years of Model Minority stereotypes? What challenges do we face as Asian-Americans in the workplace? Come to this workshop to learn more about how your Asian American identity affects you as a professional.
         </div>
       </li>
     </ul>
-    <!-- <br>
+    <br>
     <ul class="accordion" data-accordion>
       <li class="accordion-navigation">
         <a href="#breakout4">
-          <b>SASE Marketing Workshop</b>
-          <br><i>How can one create a lasting impact? Panelists share their stories and inspire attendees to pursue their goals.</i>
+          <b>SASE Marketing Workshop</b><br>
+          <i>Omer Bakshi; CSC 333</i>
         </a>
         <div id="breakout4" class="content">
-          bla
+          This year, SASE National is looking to pilot a Marketing Workshop at the Northeast Regional Conference. This workshop is aimed to teach SASE Chapters how establish their brand, and will cover the important do's and don'ts of encouraging general body involvement, partnering with outside student organizations, and utilizing social media effectively.
         </div>
       </li>
-    </ul> -->
+    </ul>
     <br>
-    <center>
-      <p><b>Look out for workshops from our corporate sponsors!</b></p>
-      <br>
-      <div class="row body-row">
-        <div class="small-4 columns">
-          <a href="https://www.bostonscientific.com/en-US/home.html" target="_blank">
-            <img src="https://www.bostonscientific.com/content/dam/bostonscientific/corporate/logos/boston-scientific-logo.png"/>
-          </a>
-        </div>
-        <div class="small-4 columns">
-          <a href="http://us.pg.com/" target="_blank">
-            <img src="http://us.pg.com/-/media/PGCOMUS/Images/Common/Header/logo%20png.png"/>
-          </a>
-        </div>
-        <div class="small-4 columns">
-          <a href="http://www.jacobs.com/" target="_blank">
-            <img src="https://5percentclubblog.files.wordpress.com/2015/02/jacobs-logo_blue.jpg"/>
-          </a>
-        </div>
-      </div>
-      <br>
-      * workshops are subject to change
-    </center>
   </div>
 </div>
 
